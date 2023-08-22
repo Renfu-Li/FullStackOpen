ef0e7e9d0b16ba554fa714322378a534deeb03ee
Renfu Li <renfu.li@outlook.com>
2023-08-21 20:21:37 -0700

    exercises 7.1-7.8

---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const useField = (name) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (e) => setValue(e.target.value);
+  const reset = () => setValue("");
+
+  return { name, value, onChange, reset };
+};
+
+export default useField;
